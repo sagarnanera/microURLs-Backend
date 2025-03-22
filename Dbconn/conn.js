93d712276255ec40a1768dfde5508ba224f8b350
sagarnanera <sagarnanera30@gmail.com>
2025-03-22 23:23:12 +0530

    refactor: improve database connection handling and add Node.js engine requirement

--- Dbconn/conn.js
+++ Dbconn/conn.js
@@ -1,22 +1,24 @@
 const mongoose = require("mongoose");
 
 const env = process.env.IS_DEV;
-const HOST = env === "true" ? process.env.MONGODB_URI_LOCAL : process.env.MONGODB_URI;
+const HOST =
+  env === "true" ? process.env.MONGODB_URI_LOCAL : process.env.MONGODB_URI;
 
-
-mongoose.connect(
-    HOST,
-    {
-        dbName: "microURLs-db",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        autoIndex: true,
-    },
-    (err) =>
-        err ?
-            console.log(err) :
-            console.log(`Connected to microURLs database ${env === "true" ? "local" : "Atlas"} \n HOST: ${HOST}`)
-);
+mongoose
+  .connect(HOST, {
+    dbName: "microURLs-db",
+    autoIndex: true
+  })
+  .then(() => {
+    console.log(
+      `Connected to microURLs database ${
+        env === "true" ? "local" : "Atlas"
+      } \n HOST: ${HOST}`
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const conn = mongoose.connection;
 

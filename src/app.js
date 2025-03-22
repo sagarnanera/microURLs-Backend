608b6bc4f85d447d700bed3607eabd66ebf2c34f
sagarnanera <sagarnanera30@gmail.com>
2025-03-22 23:12:25 +0530

    refactor: update cors, add pre-flight config

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ const passport = require("passport");
 const captchaVerify = require("../middleware/captchVerifier");
 const geoLocationMiddleware = require("../middleware/geoLocationMiddleware");
 
-
 // routes
 const { redirectUser } = require("../controllers/redirectController");
 const authRoute = require("../routes/authRoute");
@@ -19,19 +18,22 @@ const urlRoute = require("../routes/urlRoute");
 const userRoute = require("../routes/userRoute");
 const supportRoute = require("../routes/supportRoute");
 
-
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var monitor = require('express-status-monitor');
+var monitor = require("express-status-monitor");
 app.use(monitor());
 
 // view engine setup
-app.set('view engine', 'ejs');
-app.set('views', './view');
+app.set("view engine", "ejs");
+app.set("views", "./view");
 
-app.use(logger('client_addr: :req[X-Forwarded-For] - host: :req[host] - [:date[web]] ":method :url HTTP/:http-version" :status :response-time ms :res[content-length] ":referrer" ":user-agent"'));
+app.use(
+  logger(
+    'client_addr: :req[X-Forwarded-For] - host: :req[host] - [:date[web]] ":method :url HTTP/:http-version" :status :response-time ms :res[content-length] ":referrer" ":user-agent"'
+  )
+);
 
 app.use(cookieParser());
 
@@ -39,17 +41,21 @@ app.use(cookieParser());
 app.use(passport.initialize());
 require("../config/passport")(passport);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+
+app.options('*', cors());
 
 app.get("/", geoLocationMiddleware, (req, res) => {
-
-    const test_slug = randomstring.generate(16);
-    res
-        .status(200)
-        .json({
-            message: "Server is up 🛠 !!!!",
-            token: test_slug
-        });
+  const test_slug = randomstring.generate(16);
+  res.status(200).json({
+    message: "Server is up 🛠 !!!!",
+    token: test_slug
+  });
 });
 
 app.use("/api/auth", authRoute);
@@ -58,17 +64,19 @@ app.use("/api/url", urlRoute);
 app.use("/api/support", supportRoute);
 app.get("/:slug", geoLocationMiddleware, redirectUser);
 
-app.get('*', (req, res) => {
-    return res.status(404).json({ message: 'Not found, Check the URL properly !!!' });
-})
+app.get("*", (req, res) => {
+  return res
+    .status(404)
+    .json({ message: "Not found, Check the URL properly !!!" });
+});
 
 app.use((err, req, res, next) => {
-    console.log(err);
-    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-        return res.status(400).json({ message: 'Invalid JSON payload' });
-    }
+  console.log(err);
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).json({ message: "Invalid JSON payload" });
+  }
 
-    return res.status(500).json({ message: 'Internal Server Error' });
+  return res.status(500).json({ message: "Internal Server Error" });
 });
 
 module.exports = app;
